4665e86a3e66ab1d41e9d0cde41e1ec76ed972b3
Kaveh Vakili <vakilik7@gmail.com>
2023-10-06 23:12:53 -0400

    added post and patch functions

--- index.js
+++ index.js
@@ -49,13 +49,36 @@ app.get("/posts",(req,res)=>{
 //CHALLENGE 2: GET a specific post by id
 app.get("/posts/:id",(req,res)=>{
   const post=posts.find((p)=>p.id==parseInt(req.params.id));
+  if(!post)return res.status(404).json({message:"Post not found"});
+  res.json(post);
+});
+//CHALLENGE 3: POST a new post
+app.post("/posts",(res,req)=>{
+  const newId=lastId+=1;
+  const post={
+    id:newId,
+    title:req.body.title,
+    content:req.body.content,
+    author:req.body.author,
+    date:new Date(),
+  };
+  lastId=newId;
+  posts.push(post);
+  res.status(201).json(post);
+});
+//CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id",(req,res)=>{
+  const post=posts.find((p)=>p.id==parseInt(req.params.id));
+  if(!post)return res.status(404).json({message:"Post not found"});
+
+  if(req.body.title)post.title=req.body.title;
+  if(req.body.content)post.content=req.body.content;
+  if(req.body.author)post.author=req.body.author;
   
   res.json(post);
+
+
 })
-//CHALLENGE 3: POST a new post
-
-//CHALLENGE 4: PATCH a post when you just want to update one parameter
-
 //CHALLENGE 5: DELETE a specific post by providing the post id.
 
 app.listen(port, () => {
